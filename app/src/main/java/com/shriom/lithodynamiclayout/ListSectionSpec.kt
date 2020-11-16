4ed53a384f6cbdabb7573b0374834b484096c150
jay006 <shriomtripathi33@gmail.com>
2020-11-16 19:30:26 +0530

    Replaced SingleComponentSection with DataDiffSection

--- app/src/main/java/com/shriom/lithodynamiclayout/ListSectionSpec.kt
+++ app/src/main/java/com/shriom/lithodynamiclayout/ListSectionSpec.kt
@@ -1,17 +1,23 @@
 package com.shriom.lithodynamiclayout
 
 import android.graphics.Color
+import com.facebook.litho.annotations.FromEvent
+import com.facebook.litho.annotations.OnEvent
 import com.facebook.litho.sections.Children
 import com.facebook.litho.sections.SectionContext
 import com.facebook.litho.sections.annotations.GroupSectionSpec
 import com.facebook.litho.sections.annotations.OnCreateChildren
-import com.facebook.litho.sections.common.SingleComponentSection
+import com.facebook.litho.sections.common.DataDiffSection
+import com.facebook.litho.sections.common.RenderEvent
+import com.facebook.litho.widget.ComponentRenderInfo
+import com.facebook.litho.widget.RenderInfo
+import com.shriom.lithodynamiclayout.ListSectionSpec.onRender
 
 
 @GroupSectionSpec
 object ListSectionSpec {
 
-    @OnCreateChildren
+  /*  @OnCreateChildren
     fun onCreateChildren(c: SectionContext?): Children? {
         val builder = Children.create()
         for (i in 0..31) {
@@ -28,6 +34,38 @@ object ListSectionSpec {
             )
         }
         return builder.build()
+    }*/
+
+    @OnCreateChildren
+    fun onCreateChildren(c: SectionContext?): Children? {
+        return Children.create()
+            .child(
+                DataDiffSection.create<Int>(c)
+                    .data(generateData(32))
+                    .renderEventHandler(ListSection.onRender(c))
+            )
+            .build()
+    }
+
+    @OnEvent(RenderEvent::class)
+    fun onRender(c: SectionContext?, @FromEvent model: Int): RenderInfo? {
+        return ComponentRenderInfo.create()
+            .component(
+                ListItem.create(c)
+                    .color(if (model % 2 == 0) Color.WHITE else Color.LTGRAY)
+                    .title("$model. Hello, world!")
+                    .subtitle("Litho tutorial")
+                    .build()
+            )
+            .build()
+    }
+
+    private fun generateData(count: Int): List<Int>? {
+        val data: MutableList<Int> = ArrayList(count)
+        for (i in 0 until count) {
+            data.add(i)
+        }
+        return data
     }
 
 }
