7aedd861d11cfab88ecab43df802a7a1e6c78f9b
jay006 <shriomtripathi33@gmail.com>
2020-11-16 21:56:47 +0530

    Minor change.

--- app/src/main/java/com/shriom/lithodynamiclayout/ListSectionSpec.kt
+++ app/src/main/java/com/shriom/lithodynamiclayout/ListSectionSpec.kt
@@ -1,6 +1,8 @@
 package com.shriom.lithodynamiclayout
 
 import android.graphics.Color
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.facebook.litho.annotations.FromEvent
 import com.facebook.litho.annotations.OnEvent
 import com.facebook.litho.sections.Children
@@ -9,32 +11,37 @@ import com.facebook.litho.sections.annotations.GroupSectionSpec
 import com.facebook.litho.sections.annotations.OnCreateChildren
 import com.facebook.litho.sections.common.DataDiffSection
 import com.facebook.litho.sections.common.RenderEvent
+import com.facebook.litho.sections.common.SingleComponentSection
+import com.facebook.litho.sections.widget.GridRecyclerConfiguration
+import com.facebook.litho.sections.widget.ListRecyclerConfiguration
+import com.facebook.litho.sections.widget.RecyclerCollectionComponent
 import com.facebook.litho.widget.ComponentRenderInfo
 import com.facebook.litho.widget.RenderInfo
-import com.shriom.lithodynamiclayout.ListSectionSpec.onRender
+import com.facebook.litho.widget.SnapUtil.SNAP_TO_CENTER
 
 
 @GroupSectionSpec
 object ListSectionSpec {
 
-  /*  @OnCreateChildren
-    fun onCreateChildren(c: SectionContext?): Children? {
-        val builder = Children.create()
-        for (i in 0..31) {
-            builder.child(
-                SingleComponentSection.create(c)
-                    .key(i.toString())
-                    .component(
-                        ListItem.create(c)
-                            .color(if (i % 2 == 0) Color.WHITE else Color.LTGRAY)
-                            .title("$i Hello World")
-                            .subtitle("Litho tutorial")
-                            .build()
-                    )
-            )
-        }
-        return builder.build()
-    }*/
+    /*  @OnCreateChildren
+      fun onCreateChildren(c: SectionContext?): Children? {
+          val builder = Children.create()
+          for (i in 0..31) {
+              builder.child(
+                  SingleComponentSection.create(c)
+                      .key(i.toString())
+                      .component(
+                          ListItem.create(c)
+                              .color(if (i % 2 == 0) Color.WHITE else Color.LTGRAY)
+                              .title("$i Hello World")
+                              .subtitle("Litho tutorial")
+                              .build()
+                      )
+              )
+          }
+          return builder.build()
+      }*/
+/*
 
     @OnCreateChildren
     fun onCreateChildren(c: SectionContext?): Children? {
@@ -43,6 +50,60 @@ object ListSectionSpec {
                 DataDiffSection.create<Int>(c)
                     .data(generateData(32))
                     .renderEventHandler(ListSection.onRender(c))
+            )
+            .build()
+    }
+*/
+
+    @OnCreateChildren
+    fun onCreateChildren(c: SectionContext?): Children? {
+        return Children.create()
+            .child(
+                SingleComponentSection.create(c)
+                    .component(
+                        RecyclerCollectionComponent.create(c)
+                            .disablePTR(true)
+                            .recyclerConfiguration(
+                                ListRecyclerConfiguration.create()
+                                    .orientation(LinearLayoutManager.HORIZONTAL)
+                                    .reverseLayout(false).snapMode(
+                                        SNAP_TO_CENTER
+                                    ).build()
+                            )
+                            .section(
+                                DataDiffSection.create<Int>(c)
+                                    .data(generateData(32))
+                                    .renderEventHandler(ListSection.onRender(c))
+                                    .build()
+                            )
+                            .canMeasureRecycler(true).build()
+                    )
+                    .build()
+            ).child(
+                DataDiffSection.create<Int>(c)
+                    .data(generateData(32))
+                    .renderEventHandler(ListSection.onRender(c)).build()
+            )
+            .child(
+                SingleComponentSection.create(c)
+                    .component(
+                        RecyclerCollectionComponent.create(c)
+                            .disablePTR(true)
+                            .recyclerConfiguration(
+                                GridRecyclerConfiguration.create()
+                                    .numColumns(2)
+                                    .orientation(GridLayoutManager.HORIZONTAL)
+                                    .build()
+                            )
+                            .section(
+                                DataDiffSection.create<Int>(c)
+                                    .data(generateData(32))
+                                    .renderEventHandler(ListSection.onRender(c))
+                                    .build()
+                            )
+                            .canMeasureRecycler(true).build()
+                    )
+                    .build()
             )
             .build()
     }
